b0ce7edc9ae777882007ceaf908889c2d8ef70cb
Uliana1Moshkova <116569593+Uliana1Moshkova@users.noreply.github.com>
2022-11-29 20:17:52 +0200

    Create Coffee.java

---
+++ Coffee.java
@@ -0,0 +1,20 @@
+package cafe;
+
+public class Coffee implements Nameable{
+
+
+
+
+    public String grain_type;
+    public String size;
+
+    public Coffee() {
+
+    }
+
+    public Coffee(String name, String grain_type, String size) {
+
+        this.grain_type = grain_type;
+        this.size = size;
+
+    }
