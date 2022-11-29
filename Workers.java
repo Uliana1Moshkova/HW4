317827422bf287a3bc4328e1eef9398f5029cbef
Uliana1Moshkova <116569593+Uliana1Moshkova@users.noreply.github.com>
2022-11-29 20:19:10 +0200

    Create Workers.java

---
+++ Workers.java
@@ -0,0 +1,18 @@
+package cafe;
+
+     public class Workers extends Person {
+
+     protected String lastname;
+     protected int years_old;
+
+          @Override
+          public void Hi() {
+               System.out.println("Hi! What would you like to order?:>");
+          }
+
+
+          @Override
+          public String name() {
+               return null;
+          }
+     }
